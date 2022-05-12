de792f9212a79d19b4ac297e23a59af6d1d2975c
JiHyeon Jeong <100845833+JiHyeon1004@users.noreply.github.com>
2022-05-12 21:57:29 +0900

    Updated On May 12 21:57

---
+++ vue-first-app/src/components/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+<h3>aboutcomponent</h3>
+{{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AboutView',
+    props: {
+        msg: String,
+    },
+    data() {
+        return{
+            text: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
